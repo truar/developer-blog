02910bbc05dff43ecf4c7d718c3ea4d3b8dc8577
truar <thibault.ruaro@gmail.com>
2020-11-11 19:23:03 +0100

    Fix about me page

--- pages/author/_author.vue
+++ pages/author/_author.vue
@@ -6,8 +6,6 @@
       Thibault has evolved as a Scrum master in agile teams, as he cares about always improving the team he is part of. From his point of view, using serious games during team retrospectives is a key factor of success for the team evolution. Technical excellence is at the heart of his personality and he organizes “mob programming” sessions with the entire team. He also leads monthly Coding session initiative in Zenika.<br/>
       <br/>
       Thibault is an official “Spring Core” trainer and has talked about GreenIT and IT revolutions at conferences. Speaking in public is a source of motivation and helps him giving his best.<br/>
-      <br/>
-      He participated in the AXA Summit 2019, as a beta-tester in the internal testing sessions organized by the Zenika team (he even found a bug that made him win the game ;) ). He is really looking forward to working on the AXA Summit 2020, as a developer this time!
     </p>
   </div>
 </template>
