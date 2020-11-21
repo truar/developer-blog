aa6c880203420d7b68d7e196ce4e4829ce718cd1
truar <thibault.ruaro@gmail.com>
2020-11-21 18:47:29 +0100

    revert ga

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,9 +60,7 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    { src: '~plugins/ga.js', mode: 'client' }
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -86,6 +84,6 @@ export default {
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {}
+  build: {},
 
 }
