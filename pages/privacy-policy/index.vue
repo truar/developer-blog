90d024ac784182c1196308f73fa22761cd542e7e
truar <thibault.ruaro@gmail.com>
2020-11-21 21:46:02 +0100

    Add privacy policy page

---
+++ pages/privacy-policy/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Privacy Policy</h1>
+
+    <h2>Cookies</h2>
+
+    <p>Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent
+      to your browser from the website that you visit and are stored on your computer’s hard drive.</p>
+
+    <p>If you choose to opt in, I will use the following Google Analytics cookies to collect information on how you are
+      using my site:</p>
+    <ul>
+      <li>_ga</li>
+      <li>_gid</li>
+    </ul>
+
+    <p>The information collected using these cookies will allow me to see which articles are resonating with my readers
+      and enable me to improve the site and write better, more useful content.</p>
+
+    <p>Please note that choosing to opt out will in no way change your experience on the website.</p>
+
+    <p>For more general information on cookies, please read <a href="https://www.cookieconsent.com/what-are-cookies/">"What
+      Are Cookies"</a>.</p>
+
+    <h2>Links to Other Sites</h2>
+
+    <p>My website may contain links to other sites. If you click on a third-party link, you will be directed to that
+      site. Note that these external sites are not operated by me. Therefore, I strongly advise you to review the
+      Privacy Policy of these websites. I have no control over, and assume no responsibility for the content, privacy
+      policies, or practices of any third-party sites or services.</p>
+
+    <h2>Changes to This Privacy Policy</h2>
+
+    <p>I may update our Privacy Policy from time to time. Thus, I advise you to review this page periodically for any
+      changes. I will notify you of any changes by posting the new Privacy Policy on this page. These changes are
+      effective immediately, after they are posted on this page.</p>
+
+    <h2>Contact Me</h2>
+
+    <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate <a
+      href="mailto:thibault.ruaro@gmail.com">to contact me.</a></p>
+  </div>
+</template>
