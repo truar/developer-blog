f13912434bee27e62bbde6ee1c7886a4a1bfd61e
truar <thibault.ruaro@gmail.com>
2020-11-21 18:32:40 +0100

    Add google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,9 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [
+    { src: '~plugins/ga.js', mode: 'client' }
+  ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -85,4 +87,5 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {}
+
 }
